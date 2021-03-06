a2141d544b69e939f2583eb01ee4e03305ff887b
dreamSeekerYu <yupeng.bai@ucarinc.com>
2021-03-06 12:26:37 +0800

    fix🐛 引用路径指向原 element-ui 问题

--- build/config.js
+++ build/config.js
@@ -1,49 +1,78 @@
-var path = require('path');
-var fs = require('fs');
-var nodeExternals = require('webpack-node-externals');
-var Components = require('../components.json');
+/* eslint-disable semi */
+var path = require('path')
+var fs = require('fs')
+var nodeExternals = require('webpack-node-externals')
+var Components = require('../components.json')
 
-var utilsList = fs.readdirSync(path.resolve(__dirname, '../src/utils'));
-var mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'));
-var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'));
-var externals = {};
+var utilsList = fs.readdirSync(path.resolve(__dirname, '../src/utils'))
+var mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'))
+var transitionList = fs.readdirSync(
+  path.resolve(__dirname, '../src/transitions')
+)
+var externals = {}
 
 Object.keys(Components).forEach(function(key) {
-  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`;
-});
+  // eslint-disable-next-line semi
+  externals[`element-ui/packages/${key}`] = `element-form-plus/lib/${key}`
+})
 
-externals['element-ui/src/locale'] = 'element-ui/lib/locale';
+externals['element-ui/src/locale'] = 'element-form-plus/lib/locale'
 utilsList.forEach(function(file) {
-  file = path.basename(file, '.js');
-  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`;
-});
+  // eslint-disable-next-line semi
+  file = path.basename(file, '.js')
+  externals[
+    `element-ui/src/utils/${file}`
+    // eslint-disable-next-line semi
+  ] = `element-form-plus/lib/utils/${file}`
+})
 mixinsList.forEach(function(file) {
-  file = path.basename(file, '.js');
-  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`;
-});
+  // eslint-disable-next-line semi
+  file = path.basename(file, '.js')
+  externals[
+    `element-ui/src/mixins/${file}`
+    // eslint-disable-next-line semi
+  ] = `element-form-plus/lib/mixins/${file}`
+})
 transitionList.forEach(function(file) {
-  file = path.basename(file, '.js');
-  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`;
-});
+  // eslint-disable-next-line semi
+  file = path.basename(file, '.js')
+  externals[
+    `element-ui/src/transitions/${file}`
+    // eslint-disable-next-line semi
+  ] = `element-form-plus/lib/transitions/${file}`
+})
 
-externals = [Object.assign({
-  vue: 'vue'
-}, externals), nodeExternals()];
+externals = [
+  Object.assign(
+    {
+      // eslint-disable-next-line comma-dangle
+      vue: 'vue',
+    },
+    externals
+  ),
+  // eslint-disable-next-line comma-dangle
+  nodeExternals(),
+]
 
-exports.externals = externals;
+// eslint-disable-next-line semi
+exports.externals = externals
 
 exports.alias = {
   main: path.resolve(__dirname, '../src'),
   packages: path.resolve(__dirname, '../packages'),
   examples: path.resolve(__dirname, '../examples'),
-  'element-ui': path.resolve(__dirname, '../')
-};
+  // eslint-disable-next-line comma-dangle
+  'element-ui': path.resolve(__dirname, '../'),
+  // eslint-disable-next-line semi
+}
 
 exports.vue = {
   root: 'Vue',
   commonjs: 'vue',
   commonjs2: 'vue',
-  amd: 'vue'
-};
+  // eslint-disable-next-line comma-dangle
+  amd: 'vue',
+  // eslint-disable-next-line semi
+}
 
-exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date\.js/;
+exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date\.js/
